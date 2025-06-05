6683df5676baf4a9b8371430438e9364fc52f91c
Woofll <jaychristophernguyen@gmail.com>
2025-06-05 16:33:02 -0700

    Creation of helper and global files

---
+++ include/globals.hpp
@@ -0,0 +1,7 @@
+#include "main.h"
+namespace global{
+//extern pros::Motor motorName;
+//extern pros::sensorType sensorName;
+//extern pros::adi::Pneumatics pneumaticsName;
+
+}
---
+++ src/globals.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+namespace global{
+//pros::Motor motorName(portNumber, motorType);
+//pros::sensorType sensorName (portNumber);
+//pros::adi::Pneumatics pneumaticsName('portLetter', state(true/false));
+
+}
