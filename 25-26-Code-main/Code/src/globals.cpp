3acfe57d967d97879eff06a62e9f5f6eecbe0ddf
Woofll <jaychristophernguyen@gmail.com>
2025-10-13 15:53:43 -0700

    Scoring Controls v2

--- 25-26-Code-main/Code/src/globals.cpp
+++ 25-26-Code-main/Code/src/globals.cpp
@@ -4,9 +4,9 @@ namespace global{
 //pros::sensorType sensorName (portNumber);
 //pros::adi::Pneumatics pneumaticsName('portLetter', state(true/false));
 
-pros::Motor leftIntake(3, pros::v5::MotorGearset::rpm_200);
-pros::Motor rightIntake(9, pros::v5::MotorGearset::rpm_200);
-pros::Motor firstScore(1, pros::v5::MotorGearset::rpm_200);
-pros::Motor secondScore(2, pros::v5::MotorGearset::rpm_200); 
+pros::Motor intake(20, pros::v5::MotorGearset::rpm_200);
+pros::Motor score(19, pros::v5::MotorGearset::rpm_200); 
+pros::Motor topFlex(9, pros::v5::MotorGearset::rpm_200); //Flex Wheel
 
+// pros::adi::Pneumatics plate('A', false); //Starts retracted
 }
